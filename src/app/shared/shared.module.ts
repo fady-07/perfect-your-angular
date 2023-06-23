e70bc5d58d674f83425d02fe37fdb4cea892e331
Fady Okana <fadyokana11@gmail.com>
2023-06-23 15:40:23 +0100

    2 commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {RouterModule} from "@angular/router";
 
 
 
@@ -7,6 +9,10 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports:[
+    MatToolbarModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
